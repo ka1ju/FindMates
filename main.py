b95a9360fa8e7ed87f373307c00f349896c7c063
Zherebtsov Yaroslav <n0name_off@mail.ru>
2022-04-09 00:57:57 +0700

    Changed host and port

--- main.py
+++ main.py
@@ -98,5 +98,5 @@ def create_news():
 
 if __name__ == '__main__':
     db_session.global_init("db/my-mates.sqlite")
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='127.0.0.1', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
